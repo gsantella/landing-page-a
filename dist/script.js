796a1d240b59cf7956bd3dc8dea16eb46a3da7ee
gsantella <gsantella@southhills.edu>
2021-03-22 23:49:14 -0400

    more cleanup

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,12 @@
-document.querySelector('figure.iframe').onclick = () => {
+document.querySelectorAll('section.box').forEach(item => {
 
-	basicLightbox.create(`
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/Scxs7L0vhZ4" frameborder="0" allowfullscreen></iframe>
-	`).show()
+	item.addEventListener('click', event => {
+		
+		//handle click
+		basicLightbox.create(`
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/${item.dataset.youtubeId}" frameborder="0" allowfullscreen></iframe>
+		`).show()
+	
+	})
 
-}
+})
